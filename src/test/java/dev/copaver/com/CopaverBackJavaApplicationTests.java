fb0c8bc0f1697587a69d30f8046d28d90da5d3b3
Artem Krumin <trapper.programmer@gmail.com>
2023-09-23 13:02:00 +0300

    init repo

---
+++ src/test/java/dev/copaver/com/CopaverBackJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.copaver.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopaverBackJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
