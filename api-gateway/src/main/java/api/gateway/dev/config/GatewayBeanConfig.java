4fa6a89d8b3c18ac8bc76587960196779597952f
trapper <trapp.umin@gmail.com>
2024-05-14 15:29:35 +0300

    feat(email-verify-parent/README.md): added README to email-verify-parent

--- api-gateway/src/main/java/api/gateway/dev/config/GatewayBeanConfig.java
+++ api-gateway/src/main/java/api/gateway/dev/config/GatewayBeanConfig.java
@@ -4,9 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @Slf4j
 @Configuration
@@ -15,15 +13,6 @@ public class GatewayBeanConfig {
     @Bean
     @LoadBalanced
     public WebClient webClient() {
-        return WebClient.builder()
-//            .baseUrl("http://localhost:8080") //"lb://auth-service"
-//            .filter(ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
-//                log.info("Sending request to URI: {}", clientRequest.url());
-//                return Mono.just(clientRequest);
-//            })).filter(ExchangeFilterFunction.ofResponseProcessor(clientResponse -> {
-//                log.info("Received response with status code: {}", clientResponse.statusCode());
-//                return Mono.just(clientResponse);
-//            }))
-            .build();
+        return WebClient.builder().build();
     }
 }
