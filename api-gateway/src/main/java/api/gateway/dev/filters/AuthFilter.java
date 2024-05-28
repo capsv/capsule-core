00586c16a28ec9e862a84cf015e4edc2ec6e8450
trapper <trapp.umin@gmail.com>
2024-05-28 19:09:02 +0300

    feat(API-Gateway/CORSConfig): created cors config for API-Gateway

--- api-gateway/src/main/java/api/gateway/dev/filters/AuthFilter.java
+++ api-gateway/src/main/java/api/gateway/dev/filters/AuthFilter.java
@@ -1,20 +1,16 @@
 package api.gateway.dev.filters;
 
 import api.gateway.dev.dtos.responses.Credentials;
-import java.time.Duration;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 @Component
-@Slf4j
 public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
 
     private final WebClient.Builder webClientBuilder;
@@ -34,21 +30,17 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
                 return exchange.getResponse().setComplete();
 
             }
-            return isJwtValid(token)
-                .flatMap(credentials -> {
-                    if (credentials != null && credentials.getUsername() != null) {
-                        log.info("Authenticating success for {}", credentials.getUsername());
-                        return chain.filter(exchange);
-                    } else {
-                        log.info("Authentication failed");
-                        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                        return exchange.getResponse().setComplete();
-                    }
-                }).onErrorResume(e -> {
-                    log.error("Error during JWT validation: ", e);
+            return isJwtValid(token).flatMap(credentials -> {
+                if (credentials != null && credentials.getUsername() != null) {
+                    return chain.filter(exchange);
+                } else {
                     exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                     return exchange.getResponse().setComplete();
-                }).subscribeOn(Schedulers.boundedElastic());
+                }
+            }).onErrorResume(e -> {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }).subscribeOn(Schedulers.boundedElastic());
         };
     }
 
