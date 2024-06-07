ae915e6ab1ad404557b10ece038e6d92ef1fb411
trapper <trapp.umin@gmail.com>
2024-06-07 14:02:45 +0300

    feat(email-verify-service): added configuration for kafkaStringTemplate

--- auth-service/src/main/java/auth/service/dev/configs/Constants.java
+++ auth-service/src/main/java/auth/service/dev/configs/Constants.java
@@ -3,4 +3,5 @@ package auth.service.dev.configs;
 public class Constants {
 
     public static String CREATE_NEW_ACCOUNT_TOPIC = "create-new-account-topic";
+    public static String SUBMIT_VERIFY_STATUS_TOPIC = "submit-verify-status-topic";
 }
