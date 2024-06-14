645749611c86b2310f58b1f632341fe7726eab9d
trapper <trapp.umin@gmail.com>
2024-06-14 18:50:28 +0300

    feat(assay-passing-service): created AssaysControllerAdvice

--- assay-passing-service/src/main/java/org/capsule/com/configs/Constants.java
+++ assay-passing-service/src/main/java/org/capsule/com/configs/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     public static final String LEVEL_AFTER_ASSAY_TOPIC = "level-after-assay-topic";
 
     public static final String SUCCESS_MESSAGE = "assay successfully passed";
+    public static final String FAILURE_MESSAGE = "assay failed to pass";
 }
--- assay-passing-service/src/main/java/org/capsule/com/services/AssaysService.java
+++ assay-passing-service/src/main/java/org/capsule/com/services/AssaysService.java
@@ -2,26 +2,37 @@ package org.capsule.com.services;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.capsule.com.configs.Constants;
 import org.capsule.com.dtos.AssayReqst;
 import org.capsule.com.dtos.Error;
 import org.capsule.com.dtos.RatingInfoResp;
 import org.capsule.com.dtos.Wrapper;
+import org.capsule.com.services.producers.KafkaJsonProducer;
+import org.capsule.com.services.producers.KafkaStringProducer;
 import org.capsule.com.utils.exceptions.FieldsOfEntityIsNotValidException;
 import org.capsule.com.utils.tools.GenerateRatingTool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
 @Service
+@RequiredArgsConstructor
 public class AssaysService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(AssaysService.class);
+    private final KafkaStringProducer kafkaStringProducer;
+    private final KafkaJsonProducer kafkaJsonProducer;
 
     public ResponseEntity<Wrapper<RatingInfoResp>> pass(AssayReqst assayReqst,
         BindingResult bindingResult) {
         validate(bindingResult);
 
         int level = GenerateRatingTool.generate(assayReqst.assay());
+
 
         return response(level);
     }
