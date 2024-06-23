f7226b9e6e3d44a91440a600d86145cb8f4cd441
trapper <trapp.umin@gmail.com>
2024-06-23 18:40:02 +0300

    feat(tasks-service): added migrations

--- logs-service/src/main/java/org/capsule/com/logs/LogsServiceApplication.java
+++ logs-service/src/main/java/org/capsule/com/logs/LogsServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class VividLogService {
+public class LogService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VividLogService.class, args);
+		SpringApplication.run(LogService.class, args);
 	}
 
 }
